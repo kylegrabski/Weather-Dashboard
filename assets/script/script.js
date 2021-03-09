4a78d51073f285e00723931ed3992593c038f909
Kyle Grabski <kyle.grabski@gmail.com>
2021-03-08 22:01:21 -0700

    fetch current weather works

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,7 +5,7 @@ var baseurl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=$
 var $btn = document.querySelector(".btn");
 var $form = document.querySelector(".form-input");
 
-var userSearch = [];
+// var userSearch = [];
 
 var lat;
 var lon;
@@ -14,50 +14,39 @@ var lon;
 var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`;
 
 
-// search btn click
-$btn.addEventListener("click", function(e){
-  e.preventDefault();
- 
-  var search = $form.value
-  userSearch.push(search);
-  localStorage.setItem("Search", JSON.stringify(userSearch)); 
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var citySearch = $form.value
 
-  $form.value = "" 
-  return userSearch;
-});
-console.log(userSearch)
-
-
-userSearch = localStorage.getItem("Search");
-JSON.parse(userSearch);
-console.log(userSearch)
-
-
-
-// function renderSearch (data){
-// console.log(test)
-// }
+  if (citySearch) {
+    getWeather(citySearch)
+  }else
+  alert("Please Enter a City Name")
+console.log(citySearch)
+};
 
 
 
 
-var city = //user input;
-function getWeather(city) {
+
+
+// var city = //user input;
+var getWeather = function (city) {
   var currentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
   fetch(currentWeatherUrl)
     .then((data) => data.json())
     .then(function (weather) {
       console.log(weather);
+      
       var lat = weather.coord.lat;
       var lon = weather.coord.lon;
+      
       var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`;
-console.log(part)
-      // look up recorded office hours on mar 8th!!!
       fetch(onecallUrl)
         .then((data) => data.json())
         .then(function (oneCallData) {
-            console.log(oneCallData);
+            // console.log(oneCallData);
         });
     });
 }
@@ -65,6 +54,10 @@ console.log(part)
 var forecastUrl =
   "http://api.openweathermap.org/data/2.5/forecast?q=Phoenix&appid=a3db2f5a7756948aa37463f113a69ca0";
 
+
+
+
+  $btn.addEventListener('click', formSubmitHandler);
 // console.log("forecast url", forecastUrl);
 
 // getWeather("Temecula");
@@ -81,17 +74,49 @@ var forecastUrl =
 //   let data = data.json()
 //   } (edited) 
   
-//   Jeremy Collins  5:49 PM
+
 //   https://openweathermap.org/img/wn/${iconID}@2x.png
-//   5:51
+
 //   var getIcon = function (iconID) {
 //      var iconUrl = `https://openweathermap.org/img/wn/${iconID}@2x.png`;
 //      var iconEl = $("<img>").attr("alt", "Weather icon").attr("src", iconUrl);
 //      return iconEl;
 //   }; (edited) 
-//   5:52
+
 //   getIcon(weather.weather[0].id);
-//   5:53
+
 //   let iconID = weather.weather[0].id
-//   5:53
+
 //   getIcon(IconID);
+
+
+
+
+
+// old code
+// search btn click
+// $btn.addEventListener("click", function(e){
+//   e.preventDefault();
+ 
+//   getWeather($form.value);
+//   var search = $form.value
+//   userSearch.push(search);
+//   localStorage.setItem("Search", JSON.stringify(userSearch)); 
+
+//   $form.value = "" 
+
+//   return userSearch;
+// });
+// console.log(userSearch)
+
+
+// userSearch = localStorage.getItem("Search");
+// JSON.parse(userSearch);
+// console.log(userSearch)
+// console.log(userSearch.length)
+
+
+
+// function renderSearch (data){
+// console.log(test)
+// }
