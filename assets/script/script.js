3bfcad7cd78ea0293986901f3b0e7fefb328391c
Kyle Grabski <kyle.grabski@gmail.com>
2021-03-08 20:08:50 -0700

    added text file to pseudocode

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,21 @@
 var api_key = "6a2eb678a90275c6b6a6400f5f55c19b";
 
-var baseurl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid${api_key}`;
+var baseurl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${api_key}`;
 
+
+var lat;
+var lon;
+var part;
 // uv index but only takes in latitude and longitude
-var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${api_key}`;
+var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`;
 
 console.log(baseurl);
 
-var city = "Temecula";
+
+
+var city = //user input;
 function getWeather(city) {
-  var currentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q={${city}}&appid=${api_key}`;
+  var currentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
   fetch(currentWeatherUrl)
     .then((data) => data.json())
@@ -17,8 +23,8 @@ function getWeather(city) {
       console.log(weather);
       var lat = weather.coord.lat;
       var lon = weather.coord.lon;
-      var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${api_key}`;
-
+      var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`;
+console.log(part)
       // look up recorded office hours on mar 8th!!!
       fetch(onecallUrl)
         .then((data) => data.json())
@@ -31,7 +37,7 @@ function getWeather(city) {
 var forecastUrl =
   "http://api.openweathermap.org/data/2.5/forecast?q=Phoenix&appid=a3db2f5a7756948aa37463f113a69ca0";
 
-console.log(forecastUrl);
+console.log("forecast url", forecastUrl);
 
 getWeather("Temecula");
 getWeather("Gilbert");
@@ -40,17 +46,24 @@ getWeather("Gilbert");
 
 
 
-// create search bar with button
-// when button clicked, use search criteria to get weather from API
 
-// main container appears in center of screen
-// displays city name and todays date along with icon showing weather, data on temp, humidity, windspeed, and UV index
 
-// 5 card appear below main content showing 5 day forecast.
 
-// search is saved and shown in container below search bar
-
-// can be triggered on form submit(user search) as well as button click
-// build function that retrieves weather that takes in the (city)
-
-// the history should be locally stored in as a JSON array
+// function(data) {
+//   let data = data.json()
+//   } (edited) 
+  
+//   Jeremy Collins  5:49 PM
+//   https://openweathermap.org/img/wn/${iconID}@2x.png
+//   5:51
+//   var getIcon = function (iconID) {
+//      var iconUrl = `https://openweathermap.org/img/wn/${iconID}@2x.png`;
+//      var iconEl = $("<img>").attr("alt", "Weather icon").attr("src", iconUrl);
+//      return iconEl;
+//   }; (edited) 
+//   5:52
+//   getIcon(weather.weather[0].id);
+//   5:53
+//   let iconID = weather.weather[0].id
+//   5:53
+//   getIcon(IconID);
