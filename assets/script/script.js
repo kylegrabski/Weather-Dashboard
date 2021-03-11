469d8594ddf3385c10b74e7ffc025ac825ac79de
Kyle Grabski <kyle.grabski@gmail.com>
2021-03-10 23:27:39 -0700

    data for 5 day forecast

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,10 +6,7 @@ var api_key = "6a2eb678a90275c6b6a6400f5f55c19b";
 var baseurl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${api_key}`;
 
 // uv index but only takes in latitude and longitude
-var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`
-
-var forecastUrl =
-  "http://api.openweathermap.org/data/2.5/forecast?q=Phoenix&appid=a3db2f5a7756948aa37463f113a69ca0";
+var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`;
 
 // --------------------HTML ELEMENTS-----------------------------
 var $btn = document.querySelector(".btn");
@@ -18,6 +15,7 @@ var $searchCardsContainer = document.querySelector("#search-cards-container");
 var $currentWeatherContainer = document.querySelector(
   "#current-weather-container"
 );
+var $forecastCardContainer = document.querySelector("#forecast-card-container");
 
 // --------------------------------------------------------------
 var userSearch = [];
@@ -28,6 +26,10 @@ var lon;
 localStorage.clear();
 
 // var so we can put it in the event listener parameter at the bottom
+
+// --------------------------FUNCTIONS-----------------------------
+
+// -----------------SAVING SEARCH HISTORY-------------
 var formSubmitHandler = function (event) {
   event.preventDefault();
 
@@ -42,8 +44,7 @@ var formSubmitHandler = function (event) {
   return userSearch;
 };
 
-// --------------------------FUNCTIONS-----------------------------
-
+// ---------------GET WEATHER DATA---------------------------------
 function getWeather(city) {
   var currentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
@@ -68,19 +69,20 @@ function getWeather(city) {
     });
 }
 
+// ----------------CREATE SEARCH HISTORY CARD-----------------
 function createSearchCard(cityName) {
-  $searchCardsContainer.innerHTML = "";
+  // $searchCardsContainer.innerHTML = "";
   $("<div>").addClass("cards").text(cityName).appendTo($searchCardsContainer);
-
 }
 
-
-// ------------------CREATE CURRENT WEATHER CONTAINER------------------
+// ------------------CREATE CURRENT WEATHER CONTAINER-----------
 function createCurrentWeather(weather) {
   // clear container
   // clearCurrentWeather();
- $currentWeatherContainer.innerHTML = "";
+  $currentWeatherContainer.innerHTML = "";
 
+  // Weather Data
+  var weatherData = weather;
   // City Name
   var cityName = weather.name;
   $("<div>")
@@ -118,13 +120,88 @@ function createCurrentWeather(weather) {
   fetch(onecallUrl)
     .then((data) => data.json())
     .then(function (oneCallData) {
+      var uvBackground;
+
+      if (oneCallData.current.uvi <= 2) {
+        uvBackground = ".favorable";
+      } else if (oneCallData.current.uvi >= 3 && oneCallData.current.uvi <= 5) {
+        uvBackground = ".moderate";
+      } else {
+        uvBackground = ".severe";
+      }
       var $uvIndex = " UV Index: " + oneCallData.current.uvi;
 
-      return $currentWeather.append($img, tempF, humidity, windspeed, $uvIndex);
-    });
-    
-};
+      // NEED TO HAVE uvINDEX BE ITS OWN ELEMENT
+      // $uvIndex.classList.add(uvBackground);
+      console.log(uvBackground);
 
+      $currentWeather.append($img, tempF, humidity, windspeed, $uvIndex);
+      createForecast(weatherData);
+    });
+}
+
+// -------------------CREATE 5 DAY FORECAST -------------------
+function createForecast(cityName) {
+  var city = cityName.name;
+
+  var lon = cityName.coord.lon;
+  var lat = cityName.coord.lat;
+  var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`;
+  fetch(onecallUrl)
+    .then((data) => data.json())
+    .then(function (onecallUrlData) {
+      // get 5 day forecast showing
+
+      console.log(onecallUrlData);
+
+      // Date Variables
+      var tempArr = [];
+      var displayDateArr = [];
+      var humidityArr = [];
+      var iconArr = [];
+
+      for (let i = 0; i <= 4; i++) {
+        // Get Date Data
+        var newDisplay = Date(onecallUrlData.daily[i].dt);
+        displayDateArr.push(newDisplay);
+
+        // Get Temp Data
+        // Convert Kelvin to Farenheit
+        tempArr.push(
+          Math.floor(1.8 * (onecallUrlData.daily[i].temp.day - 273) + 32)
+        );
+
+        // Humidity Data
+        humidityArr.push(onecallUrlData.daily[i].humidity);
+
+        // Icon Data
+        // iconArr.push(onecallUrlData.daily[i].weather[i].icon);
+        // var iconSource = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+        // var $img = document.createElement("img");
+        // $img.setAttribute("src", iconSource);
+      }
+      console.log("Temperature" + tempArr);
+      console.log("Date" + displayDateArr);
+      console.log("Humidity" + humidityArr);
+
+      
+      
+      
+      // ***********Create Forecast Cards*************
+      for (let j = 0; j <= 4; j++) {
+
+        $forecastCardContainer.append(document.createElement("div").classList.add(`forecast-card${j}`));   
+        // var forecast1 = document.querySelector(".forecast-card1`");
+        
+
+      };
+      // Date.prototype.toDateString(displayDate)
+
+      // var displayDate = date.toLocaleString('en-US',{month: 'numeric', day:'2-digit', year:'numeric'});
+
+      // icon
+    });
+}
 
 $btn.addEventListener("click", formSubmitHandler);
 
